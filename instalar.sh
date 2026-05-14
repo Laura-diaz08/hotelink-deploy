3982db17ed93e040e10774b997f3a40170236249
Laura-diaz08 <lauritadr0808@gmail.com>
2026-05-14 20:22:21 +0200

    Archivos modificados

--- instalar.sh
+++ instalar.sh
@@ -4,14 +4,12 @@ echo "=============================="
 echo "  INSTALANDO HOTELINK"
 echo "=============================="
 
-# Instalar curl si no está
 if ! command -v curl &> /dev/null; then
     echo "Instalando curl..."
     sudo apt-get update
     sudo apt-get install -y curl
 fi
 
-# Instalar Docker si no está
 if ! command -v docker &> /dev/null; then
     echo "Instalando Docker..."
     curl -fsSL https://get.docker.com -o get-docker.sh
@@ -22,10 +20,8 @@ else
     echo "Docker ya está instalado."
 fi
 
-# Permisos Docker
 sudo chmod 666 /var/run/docker.sock
 
-# Instalar Git si no está
 if ! command -v git &> /dev/null; then
     echo "Instalando Git..."
     sudo apt-get update
@@ -35,7 +31,6 @@ else
     echo "Git ya está instalado."
 fi
 
-# Clonar repositorios
 echo "Clonando repositorios..."
 
 if [ ! -d "hotelink-backend" ]; then
@@ -54,29 +49,15 @@ else
     cd hotelink-frontend && git pull && cd ..
 fi
 
-# Descargar backup si no existe
-if [ ! -f "hotelink_backup.sql" ]; then
-    echo "Descargando datos..."
-    git clone https://github.com/Laura-diaz08/hotelink-data.git
-    cp hotelink-data/hotelink_backup.sql .
-    rm -rf hotelink-data
-    echo "Datos descargados."
-else
-    echo "Backup ya existe."
-fi
-
-# Levantar todo
 echo "Levantando Hotelink..."
 sudo docker compose up --build -d
 
-# Esperar a que postgres esté listo
 echo "Esperando a que la base de datos esté lista..."
 sleep 15
 
-# Importar datos
 echo "Importando datos..."
 sudo docker exec -i hotelink-postgres psql -U hotelink_user -d hotelink -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
-sudo docker cp hotelink_backup.sql hotelink-postgres:/tmp/
+sudo docker cp hotelink-data/hotelink_backup.sql hotelink-postgres:/tmp/
 sudo docker exec -i hotelink-postgres psql -U hotelink_user -d hotelink -f /tmp/hotelink_backup.sql
 sudo docker restart hotelink-backend
 
